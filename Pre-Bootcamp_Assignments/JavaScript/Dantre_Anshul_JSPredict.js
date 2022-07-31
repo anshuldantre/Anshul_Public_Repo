b877e7b230d87f373c5c67f167e5fd4f20ac7d75
Anshul Dantre <anshul.dantre@gmail.com>
2022-07-31 13:58:35 -0400

    First JS assignment

---
+++ Pre-Bootcamp_Assignments/JavaScript/Dantre_Anshul_JSPredict.js
@@ -0,0 +1,28 @@
+//Predict the ouptut of following JS 
+//Predict1
+// I was born in 1980
+
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    }
+
+
+//predict2
+// I was born in 1980
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+    }
+    
+
+//Predict3
+//Summing Numbers!
+//num1 is: 10
+//num2 is: 20
+//30
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
