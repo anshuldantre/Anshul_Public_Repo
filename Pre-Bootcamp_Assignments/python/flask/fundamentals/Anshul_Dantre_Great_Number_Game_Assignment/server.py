193b259365cd610a1515be965a48ba1975f29d62
ANSHUL DANTRE <anshul.dantre@gmail.com>
2023-01-29 21:24:08 -0500

    game number raw code

--- Pre-Bootcamp_Assignments/python/flask/fundamentals/Anshul_Dantre_Great_Number_Game_Assignment/server.py
+++ Pre-Bootcamp_Assignments/python/flask/fundamentals/Anshul_Dantre_Great_Number_Game_Assignment/server.py
@@ -1,15 +1,31 @@
+import random
 from flask import Flask, render_template, redirect, session, request
 app=Flask(__name__)
 
 app.secret_key = ("GreatNumberSession")
+# 
 
 @app.route('/')
 def index():
+    if 'RandomNum' not in session:
+        session['RandomNum'] = random.randint(1,100)
+    print(session['RandomNum'])
     return render_template("index.html")
 
 @app.route('/userform', methods=['POST'])
 def userinput():
     print(request.form)
+    print(int(request.form['numinput']))
+    print('--------------------------------------------------')
+    if session['RandomNum'] > int(request.form['numinput']):
+        session['msg']='Too Low'
+        session['col']=1
+    elif session['RandomNum'] < int(request.form['numinput']):
+        session['msg']='Too High'
+        session['col']=2
+    else:
+        session['msg']= f'{request.form["numinput"]} Was the number'
+        session['col']=3
     return redirect('/')
 
 if __name__== '__main__':
