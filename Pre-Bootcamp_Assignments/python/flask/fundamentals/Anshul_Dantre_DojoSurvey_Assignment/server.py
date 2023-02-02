d80517f0c5fa3debd8dd33082b532d090dd60c76
Anshul Dantre <anshul.dantre@gmail.com>
2023-02-01 22:01:22 -0500

    DOJO Survey assignment

--- Pre-Bootcamp_Assignments/python/flask/fundamentals/Anshul_Dantre_DojoSurvey_Assignment/server.py
+++ Pre-Bootcamp_Assignments/python/flask/fundamentals/Anshul_Dantre_DojoSurvey_Assignment/server.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+
+app.secret_key = 'DojoSurveySecretKey'
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['username'] = request.form['username']
+    session['location'] = request.form['location']
+    session['language'] = request.form['language']
+    session['comments'] = request.form['comments']
+    return redirect('/result')
+
+@app.route('/result')
+def result():
+    return render_template("result.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
