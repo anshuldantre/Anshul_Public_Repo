ac6cd666e738b26e35d4fd2f5c8cd0a24cc43933
Anshul Dantre <anshul.dantre@gmail.com>
2023-02-18 20:00:06 -0500

    Add a new user in db by taking inputs from webform

--- Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/First_Flask/friend.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/First_Flask/friend.py
@@ -11,7 +11,7 @@ class Friend:
         self.created_at = data['created_at']
         self.updated_at = data['updated_at']
 
-    # Now we use class methof to query our database
+    # Now we use class method to query our database
     @classmethod
     def get_all(cls):
         query = 'SELECT * FROM friends;'
@@ -23,3 +23,8 @@ class Friend:
         for friend in results:
             friends.append( cls(friend) )
         return friends
+
+    @classmethod
+    def save(cls, data):
+        query = "INSERT INTO friends (first_name, last_name, occupation, created_at, updated_at) VALUES ( %(fname)s, %(lname)s, %(occ)s, now(), now() )"
+        return connectToMySQL('first_flask').query_db(query, data)
--- Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/First_Flask/server.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/First_Flask/server.py
@@ -6,8 +6,18 @@ app = Flask(__name__)
 def index():
     # call the get all classmethod to get all the friends
     friends = Friend.get_all()
-    print(friends)
-    return render_template("index.html")
+    # print(friends)
+    return render_template("index.html", all_friends = friends)
+
+@app.route("/create_friend", methods=["POST"])
+def create_friend():
+    data = {
+        "fname" : {request.form["fname"]},
+        "lname" : {request.form["lname"]},
+        "occ" : {request.form["occ"]}
+    }
+    Friend.save(data)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
