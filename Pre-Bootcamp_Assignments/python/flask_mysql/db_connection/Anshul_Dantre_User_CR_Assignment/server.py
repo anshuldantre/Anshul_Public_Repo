e36c53402ef8d06daf27f9477c8ee6b351da80bc
Anshul Dantre <anshul.dantre@gmail.com>
2023-02-19 17:00:14 -0500

    User assignment with all CRUD functionality

--- Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/Anshul_Dantre_User_CR_Assignment/server.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/Anshul_Dantre_User_CR_Assignment/server.py
@@ -7,6 +7,11 @@ def index():
     users = User.get_all_users()
     return render_template("index.html", all_users = users)
 
+@app.route("/show_user/<int:id>")
+def show_user(id):
+    result = User.show_user(id)
+    return render_template("show_user.html", user_detail = result)
+
 @app.route("/new_user")
 def new_user():
     return render_template("new_user.html")
@@ -14,6 +19,23 @@ def new_user():
 @app.route("/add_user", methods=["POST"])
 def add_user():
     User.add_new_user(request.form)
+    return redirect("/")
+
+@app.route("/update/<int:id>")
+def update(id):
+    result = User.show_user(id)
+    return render_template("update_user.html", user_detail = result)
+
+@app.route("/update_user", methods=["POST"])
+def update_user():
+    print(request.form)
+    User.update_user(request.form)
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    print("within delete")
+    User.delete_user(id)
     return redirect("/")
 
 if __name__ == "__main__":
--- Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/Anshul_Dantre_User_CR_Assignment/user.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/Anshul_Dantre_User_CR_Assignment/user.py
@@ -1,6 +1,7 @@
 from mysqlconnection import connectToMySQL
 
 class User:
+    DB = "user_schema"
     def __init__(self, data):
         self.id = data["id"]
         self.first_name = data["first_name"]
@@ -13,12 +14,39 @@ class User:
     def get_all_users(cls):
         users = []
         query = "SELECT * FROM users"
-        results = connectToMySQL('user_schema').query_db(query)
+        results = connectToMySQL(cls.DB).query_db(query)
         for rows in results:
             users.append( cls(rows) )
         return users
-    
+
+    @classmethod
+    def show_user(cls, id):
+        query = "SELECT * FROM users WHERE id = %(id)s"
+        data = {"id": id}
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        user = []
+        for all in result:
+            user.append(cls(all))
+        return user
+
     @classmethod
     def add_new_user(cls, data):
         query = "INSERT INTO users (first_name, last_name, email, created_at, updated_at) VALUES ( %(fname)s, %(lname)s,%(email)s, NOW(), NOW() )"
-        return connectToMySQL('user_schema').query_db(query, data)
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def update_user(cls, data):
+        query = """UPDATE users 
+                    SET first_name = %(fname)s,
+                        last_name = %(lname)s,
+                        email = %(email)s
+                    WHERE id = %(id)s
+                    """
+        data = {"id": data['id'], "fname" : data['fname'], "lname": data['lname'], "email": data['email']}
+        return connectToMySQL(cls.DB).query_db(query, data)
+
+    @classmethod
+    def delete_user(cls, data):
+        query = "DELETE FROM users WHERE id = %(id)s"
+        data = {"id" : data}
+        return connectToMySQL(cls.DB).query_db(query, data)
