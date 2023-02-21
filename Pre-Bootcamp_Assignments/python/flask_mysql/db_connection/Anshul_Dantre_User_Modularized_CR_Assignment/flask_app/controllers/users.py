d1d04de72859038fbee21b606cf547866620cf39
Anshul Dantre <anshul.dantre@gmail.com>
2023-02-20 19:35:13 -0500

    Modularized code

---
+++ Pre-Bootcamp_Assignments/python/flask_mysql/db_connection/Anshul_Dantre_User_Modularized_CR_Assignment/flask_app/controllers/users.py
@@ -0,0 +1,39 @@
+from flask_app import app
+from flask import Flask, render_template, redirect, session, request
+from flask_app.models.users import User
+
+@app.route("/")
+def index():
+    users = User.get_all_users()
+    return render_template("index.html", all_users = users)
+
+@app.route("/show_user/<int:id>")
+def show_user(id):
+    result = User.show_user(id)
+    return render_template("show_user.html", user_detail = result)
+
+@app.route("/new_user")
+def new_user():
+    return render_template("new_user.html")
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    User.add_new_user(request.form)
+    return redirect("/")
+
+@app.route("/update/<int:id>")
+def update(id):
+    result = User.show_user(id)
+    return render_template("update_user.html", user_detail = result)
+
+@app.route("/update_user", methods=["POST"])
+def update_user():
+    print(request.form)
+    User.update_user(request.form)
+    return redirect("/")
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    print("within delete")
+    User.delete_user(id)
+    return redirect("/")
