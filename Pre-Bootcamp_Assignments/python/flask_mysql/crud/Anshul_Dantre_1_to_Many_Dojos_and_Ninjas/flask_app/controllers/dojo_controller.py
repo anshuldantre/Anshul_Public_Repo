388532a11eba10a6141671b03c0c39d47a80aa3d
Anshul Dantre <anshul.dantre@gmail.com>
2023-02-26 22:50:36 -0500

    full part2 of the assignment

--- Pre-Bootcamp_Assignments/python/flask_mysql/crud/Anshul_Dantre_1_to_Many_Dojos_and_Ninjas/flask_app/controllers/dojo_controller.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/crud/Anshul_Dantre_1_to_Many_Dojos_and_Ninjas/flask_app/controllers/dojo_controller.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, redirect, session, request
 from flask_app.models.dojo_model import Dojo
 
 @app.route("/")
+def index():
+    return redirect("/dojos")
+
+@app.route("/dojos")
 def dojos():
     all_dojos = Dojo.get_all_dojos()
     return render_template("dojos.html", dojo_list = all_dojos)
@@ -10,9 +14,17 @@ def dojos():
 @app.route("/add_new_dojo", methods=["POST"])
 def add_new_dojo():
     Dojo.create_new_dojo(request.form['dname'])
-    return redirect("/")
+    return redirect("/dojos")
 
-@app.route("/get_all_ninjas_in_dojo/<int:id>")
-def get_all_ninjas_in_dojo(id):
-    ninja_list = Dojo.get_all_ninjas_in_a_dojo(id)
+@app.route("/show_ninja/<int:id>")
+def show_ninja(id):
+    ninja_list = Dojo.show_ninja(id)
     return render_template("show_ninjas.html", ninja_list = ninja_list)
+
+@app.route("/delete_dojo/<int:id>")
+def delete_dojo(id):
+    print(f"within controller {id}")
+    result = Dojo.deletedojo(id)
+    all_dojos = Dojo.get_all_dojos()
+    return render_template("dojos.html", dojo_list = all_dojos)
+
