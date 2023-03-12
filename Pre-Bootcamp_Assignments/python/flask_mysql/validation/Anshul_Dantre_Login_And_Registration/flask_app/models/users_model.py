7448ecccc721e88bf0e218c493bdf391fe247b1c
Anshul Dantre <anshul.dantre@gmail.com>
2023-03-12 15:54:17 -0400

    Working Login and registration page

--- Pre-Bootcamp_Assignments/python/flask_mysql/validation/Anshul_Dantre_Login_And_Registration/flask_app/models/users_model.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/validation/Anshul_Dantre_Login_And_Registration/flask_app/models/users_model.py
@@ -1,9 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
 from flask import flash
-# from flask_bcrypt import Bcrypt
 import re
 
-# bcrypt = Bcrypt(app)
 EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User:
@@ -20,25 +18,42 @@ class User:
     @staticmethod
     def validate_user(user):
         is_valid = True
-        if len(user['first_name']) <= 0 or len(user['last_name']) <= 0 or len(user['email']) <= 0 or len(user['password']) <= 0 or len(user['confirm_password']) <= 0:
-            flash("All fields are mandatory!")
+        
+        if len(user['first_name'].strip()) <= 0 or len(user['last_name'].strip()) <= 0 or len(user['email'].strip()) <= 0 or len(user['password'].strip()) <= 0 or len(user['confirm_password'].strip()) <= 0:
+            flash("All fields are mandatory!","register")
             is_valid=False
         if len(user['first_name']) < 2 or len(user['last_name']) < 2:
-            flash("First Name and Last Name should be more than 2 characters!")
+            flash("First and Last Name should be more than 2 characters!","register")
             is_valid=False
-        if not user['first_name'].isaplha() or not user['last_name'].isaplha():
-            flash("First Name and Last Name should be letters only!")
+        if not ( user['first_name'].isalpha() and user['last_name'].isalpha() ):
+            flash("First Name and Last Name should be letters only!","register")
             is_valid=False
         if user['password'] != user['confirm_password']:
-            flash("Password and Confirm Password do not Match!")
+            flash("Password and Confirm Password do not Match!","register")
             is_valid=False
         if len(user['password']) < 8:
-            flash("Password should be atleast 8 characters long!")
+            flash("Password should be atleast 8 characters long!","register")
             is_valid=False
         if not EMAIL_REGEX.match(user['email']):
-            flash("Invalid Email !")
+            flash("Invalid Email !","register")
             is_valid=False
+        if len(user['email']) > 0:
+            logged_in_user = User.validate_email(user['email'])
+            if logged_in_user:
+                if logged_in_user.id > 0:
+                    flash("Email already resgistered !","register")
+                    is_valid=False
         return is_valid
+
+    @classmethod
+    def validate_email(cls, email):
+        lower_email = email.lower()
+        query="SELECT * FROM users WHERE lower(email) = %(email)s"
+        data = {"email" : lower_email}
+        logged_in_user = connectToMySQL(cls.db).query_db(query, data)
+        if len(logged_in_user) < 1:
+            return False
+        return cls( logged_in_user[0] )
 
     @classmethod
     def resigter_user(cls, details):
@@ -50,3 +65,12 @@ class User:
             }
         results = connectToMySQL(cls.db).query_db(query, data)
         return results
+
+    @classmethod
+    def get_user_by_id(cls, id):
+        query="SELECT * FROM users WHERE id = %(id)s"
+        data = {"id" : id}
+        logged_in_user = connectToMySQL(cls.db).query_db(query, data)
+        if len(logged_in_user) < 1:
+            return False
+        return cls( logged_in_user[0] )
--- Pre-Bootcamp_Assignments/python/flask_mysql/validation/Anshul_Dantre_Login_And_Registration/server.py
+++ Pre-Bootcamp_Assignments/python/flask_mysql/validation/Anshul_Dantre_Login_And_Registration/server.py
@@ -1,7 +1,5 @@
 from flask_app import app
 from flask_app.controllers import login_controller
 
-print("Hello World")
-
 if __name__=="__main__":
     app.run(debug=True)
