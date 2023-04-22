6c50e02e98dc64c64ea647321140555f781e7692
Anshul Dantre <anshul.dantre@gmail.com>
2023-04-22 17:33:00 -0400

    New Project

---
+++ Pre-Bootcamp_Assignments/python/My_Silo_Project/flask_app/controllers/login_controller.py
@@ -0,0 +1,28 @@
+from flask_app import app
+from flask import Flask, render_template, request, redirect, session
+from flask_app.models.user_model import User
+from flask_bcrypt import Bcrypt
+from flask import flash
+
+bcrypt = Bcrypt(app)
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+@app.route("/register_user", methods=["POST"])
+def register_user():
+    print(request.form)
+    if not User.validate_user(request.form):
+        print("validating user")
+        return redirect("/")
+    print("validated")
+    pw_hash = bcrypt.generate_password_hash(request.form['password'])
+    data = {"first_name": request.form['first_name'],
+            "last_name": request.form['last_name'],
+            "email": request.form['email'],
+            "password": pw_hash
+            }
+    logged_in_user_id = User.resigter_user(data)
+    session['logged_in_user_id'] = logged_in_user_id
+    return redirect("/routines")
